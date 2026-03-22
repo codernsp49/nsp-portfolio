438d45e032c87f9e0ddc19b29de0cf99cef1a1be
codernsp49 <codernsp49@gmail.com>
2026-03-22 02:59:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+const texts = [
+  "Web Developer 💻",
+  "Cyber Learner 🔐",
+  "Building Cool Stuff 🚀"
+];
 
+let i = 0;
+let j = 0;
+let currentText = "";
+let isDeleting = false;
+
+function type() {
+  currentText = texts[i];
+
+  if (!isDeleting) {
+    document.getElementById("typing").innerHTML =
+      currentText.substring(0, j++);
+  } else {
+    document.getElementById("typing").innerHTML =
+      currentText.substring(0, j--);
+  }
+
+  if (j == currentText.length) isDeleting = true;
+  if (j == 0) {
+    isDeleting = false;
+    i = (i + 1) % texts.length;
+  }
+
+  setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
