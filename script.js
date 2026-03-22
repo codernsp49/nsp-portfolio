99867347cc3aed757747943c86d8950e6293104e
codernsp49 <codernsp49@gmail.com>
2026-03-22 04:00:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,25 @@ document.addEventListener("DOMContentLoaded", function() {
   let j = 0;
   let currentText = "";
   let isDeleting = false;
+// SCROLL REVEAL 🔥
+const reveals = document.querySelectorAll(".reveal");
 
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+
+// first load pe bhi run karo
+revealOnScroll();
   function type() {
     currentText = texts[i];
 
